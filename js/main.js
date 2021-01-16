f0fb969fc4943183be6a8b98b0e486f5e82c7f6d
zz <1457586680@qq.com>
2021-01-16 12:33:24 +0800

    第一次提交

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    var $banner_bar = $(".banner_bar")
+    var $imgBox = $(".imgBox")
+    var $imgNum = $(".imgNum")
+    var img_Width = 811  //图片的宽度
+    var TIME = 400   //翻页的速度
+    var ITEM_TIME = 20 //单元移动的间隔时间
+    var imgCount = $imgNum.length
+    var index = 0//当前下标
+    var moving = false//标识是否正在翻页
+    imgMove()
+
+    function imgMove(){
+        
+    }
+})
